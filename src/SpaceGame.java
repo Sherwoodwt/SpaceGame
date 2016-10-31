ba7b0cbe2a9d6b449b7fdda2ba9640d790d9003c
Will Sherwood <Will Sherwood>
2016-10-30 22:33:02 -0700

    Fixed game loop, no interpolation, but handles timing better then before

--- src/SpaceGame.java
+++ src/SpaceGame.java
@@ -1,3 +1,4 @@
+import java.util.Timer;
 
 public class SpaceGame {
 
@@ -5,47 +6,29 @@ public class SpaceGame {
 	public static final int SKIP_TICKS = 1000 / FRAME_RATE;
 	public static final int MAX_SKIP = 5;
 	
+	private static GameBox box;
+	private static boolean isRunning;
+	private static Timer timer;
+	
 	public static void main(String[] args)
 	{
-		GameBox box = new GameBox();
-		
-		boolean running = true;
-		int numberLoops = 0;
-		double interpolation = 0;
-		long time = System.nanoTime();
-		
-		while(running)
-		{
-			numberLoops = 0;
-			
-			/*
-			 * update and get user input. No more than MAX_SKIP times in a row,
-			 * and you only do it every 20 milliseconds (1000/60).
-			 * 
-			 * Basically, if 20 milliseconds hasn't gone by, just draw again.
-			 * If 20 milliseconds has gone by, just update again, but only 5 times before a draw is
-			 * required.
-			 */
-			while(System.nanoTime() > time && numberLoops < MAX_SKIP)
-			{
-				box.update();
-				try{
-					Thread.sleep(1);
-				}catch(Exception e){
-					System.err.println("game broken");
-				}
-				
-				time += SKIP_TICKS;
-				numberLoops++;
-			}
-			
-			/*
-			 * interpolation is a value to draw the inbetween-update position of objects.
-			 * Current time - time is less than 0, but absolute value is less than SKIP_TICKS, so
-			 * interpolation is basically what portion of SKIP_TICKS amount of time has passed.
-			 */
-			interpolation = (double)((System.nanoTime() -time) + SKIP_TICKS)/((double)(SKIP_TICKS));
+		box = new GameBox();
+		isRunning = true;
+		timer = new Timer();
+		timer.schedule(new Loop(), 0, 1000/60);
+	}
+	
+	private static class Loop extends java.util.TimerTask
+	{
+		@Override
+		public void run() {
+			box.update();
 			box.draw();
+			
+			if(!isRunning)
+			{
+				timer.cancel();
+			}
 		}
 	}
 }
